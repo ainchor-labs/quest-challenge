19b54af3b201b67e5859994e95b0f04f14826bb2
Steven <steven@ainchor.co>
2024-05-20 19:44:49 -0400

    Updated main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,19 +1,19 @@
 # Specify the required provider and version
 terraform {
+  backend "s3" {
+    bucket         = "quest-challenge-tfstate"
+    key            = "terraform.tfstate"
+    encrypt        = true
+  }
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source       = "hashicorp/aws"
+      version      = "~> 4.0"
     }
   }
 }
 
 # Configure the AWS provider
 provider "aws" {
-  region           = "us-east-1"  # Replace with your desired AWS region
-  backend "s3" {
-    bucket         = "quest-challenge-tfstate"
-    key            = "terraform.tfstate"
-    encrypt        = true
-  }
+  region = "us-east-1"  # Replace with your desired AWS region
 }
